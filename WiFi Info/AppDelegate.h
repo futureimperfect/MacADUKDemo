0d6940f7f0092664d12683fa8ae6384786d46a88
James Barclay <jbarclay@duosecurity.com>
2016-02-08 19:02:45 +0000

    Initial commit.

--- WiFi Info/AppDelegate.h
+++ WiFi Info/AppDelegate.h
@@ -10,6 +10,24 @@
 
 @interface AppDelegate : NSObject <NSApplicationDelegate>
 
+@property (weak) IBOutlet NSTextField *rssiLabel;
+@property (weak) IBOutlet NSTextField *noiseLabel;
+@property (weak) IBOutlet NSTextField *interfaceNameLabel;
+@property (weak) IBOutlet NSTextField *txRateLabel;
+@property (weak) IBOutlet NSTextField *channelLabel;
+@property (weak) IBOutlet NSLevelIndicator *rssiLevelIndicator;
+
+- (IBAction)openNetworkPreferences:(id)sender;
+
+#define NSColorFromRGB(rgbValue) [NSColor \
+colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 \
+green:((float)((rgbValue & 0xFF00) >> 8))/255.0 \
+blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
+
+#define NSColorFromRGBWithAlpha(rgbValue,a) [NSColor \
+colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 \
+green:((float)((rgbValue & 0xFF00) >> 8))/255.0 \
+blue:((float)(rgbValue & 0xFF))/255.0 alpha:a]
 
 @end
 
--- WiFi Info/AppDelegate.m
+++ WiFi Info/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <CoreWLAN/CoreWLAN.h>
 
 @interface AppDelegate ()
 
@@ -15,12 +16,97 @@
 
 @implementation AppDelegate
 
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
     // Insert code here to initialize your application
+    _rssiLevelIndicator.minValue = -90;
+    _rssiLevelIndicator.maxValue = -30;
+    _rssiLevelIndicator.warningValue = -80;
+    _rssiLevelIndicator.criticalValue = -85;
+    _rssiLevelIndicator.levelIndicatorStyle = NSContinuousCapacityLevelIndicatorStyle;
+    [self updateLabels:nil];
+    [NSTimer scheduledTimerWithTimeInterval:1.0f target:self selector:@selector(updateLabels:) userInfo:nil repeats:YES];
 }
 
-- (void)applicationWillTerminate:(NSNotification *)aNotification {
+- (void)applicationWillTerminate:(NSNotification *)aNotification
+{
     // Insert code here to tear down your application
+}
+
+- (IBAction)openNetworkPreferences:(id)sender
+{
+    [[NSWorkspace sharedWorkspace] openFile:@"/System/Library/PreferencePanes/Network.prefPane"];
+}
+
+- (NSAttributedString *)buildLabelColor:(NSString *)key withValue:(long)value
+{
+    NSDictionary *attrs = [self getRSSILabelColor:value];
+    NSMutableAttributedString *attributedText = [[NSMutableAttributedString alloc] init];
+    NSAttributedString *beginning = [[NSAttributedString alloc] initWithString:key];
+    NSAttributedString *end = [[NSAttributedString alloc] initWithString:[NSString stringWithFormat:@"%ld dBm", value] attributes:attrs];
+    [attributedText appendAttributedString:beginning];
+    [attributedText appendAttributedString:end];
+    
+    return attributedText;
+}
+
+- (NSAttributedString *)buildAttributedString:(NSString *)key withValue:(NSString *)value
+{
+    const CGFloat fontSize = 15;
+    NSDictionary *attrs = @{NSFontAttributeName : [NSFont systemFontOfSize:fontSize]};
+    NSMutableAttributedString *attributedText = [[NSMutableAttributedString alloc] init];
+    NSAttributedString *beginning = [[NSAttributedString alloc] initWithString:key];
+    NSAttributedString *end = [[NSAttributedString alloc] initWithString:value attributes:attrs];
+    [attributedText appendAttributedString:beginning];
+    [attributedText appendAttributedString:end];
+    
+    return attributedText;
+}
+
+- (NSDictionary *)getRSSILabelColor:(long)val
+{
+    unsigned long long newVal;
+    
+    if (val < -80) {
+        newVal = 0xFFB238;
+    } else if (val < -70) {
+        newVal = 0xF19143;
+    } else if (val < 60) {
+        newVal = 0xFF773D;
+    } else {
+        newVal = 0xF55536;
+    }
+    
+    const CGFloat fontSize = 15;
+    return @{NSForegroundColorAttributeName : NSColorFromRGBWithAlpha(newVal, 0.75),
+             NSFontAttributeName : [NSFont systemFontOfSize:fontSize]};
+}
+
+- (void)updateLabels:(id)sender
+{
+    CWWiFiClient *client = [CWWiFiClient sharedWiFiClient];
+    long rssi = (long)client.interface.rssiValue;
+    long noise = (long)client.interface.noiseMeasurement;
+    NSString *interfaceName = client.interface.interfaceName;
+    double transmitRate = client.interface.transmitRate;
+    NSUInteger channel = client.interface.wlanChannel.channelNumber;
+    
+    _rssiLevelIndicator.integerValue = rssi;
+    
+    NSAttributedString *rssiAttributedText = [self buildLabelColor:@"RSSI:  " withValue:rssi];
+    [_rssiLabel setAttributedStringValue:rssiAttributedText];
+    
+    NSAttributedString *noiseAttributedText = [self buildAttributedString:@"Noise:  " withValue:[NSString stringWithFormat:@"%ld dBM", noise]];
+    [_noiseLabel setAttributedStringValue:noiseAttributedText];
+    
+    NSAttributedString *interfaceNameAttributedText = [self buildAttributedString:@"Interface Name:  " withValue:interfaceName];
+    [_interfaceNameLabel setAttributedStringValue:interfaceNameAttributedText];
+    
+    NSAttributedString *txRateAttributedText = [self buildAttributedString:@"Tx Rate:  " withValue:[NSString stringWithFormat:@"%0.0f Mbps", transmitRate]];
+    [_txRateLabel setAttributedStringValue:txRateAttributedText];
+    
+    NSAttributedString *channelAttributedText = [self buildAttributedString:@"Channel:  " withValue:[NSString stringWithFormat:@"%lu", (unsigned long)channel]];
+    [_channelLabel setAttributedStringValue:channelAttributedText];
 }
 
 @end
